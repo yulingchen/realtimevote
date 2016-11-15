965cdcc59c11054cb61e6e9cf24e945d1af3df95
yulingchen <yulingchen@vip.qq.com>
2016-11-15 15:12:42 +0800

    update

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,21 +1,12 @@
 module.exports.mongoose = {
-
-
-  // The Mongo connection URI
-  // (see https://github.com/mikermcneil/sails-hook-orm-mongoose/blob/master/README.md#configuration for more info)
-  //
-  uri: 'mongodb://chenyl:BwvxiBzhc3XME@dds-bp1788f76b9aaa842.mongodb.rds.aliyuncs.com:3717/vote,mongodb://chenyl:BwvxiBzhc3XME@dds-bp1788f76b9aaa841.mongodb.rds.aliyuncs.com:3717',
-
-
-  // Optionally, you can provide options to pass in as the second argument to `mongoose.connect()`
-  // (see https://github.com/mikermcneil/sails-hook-orm-mongoose/blob/master/README.md#configuration for more info)
-  //
+  uri: 'mongodb://chenyl:BwvxiBzhc3XME@dds-bp1788f76b9aaa842.mongodb.rds.aliyuncs.com:3717,dds-bp1788f76b9aaa841.mongodb.rds.aliyuncs.com:3717/vote?replicaSet=mgset-1721345',
   connectionOpts: {
-	db: { native_parser: true },
-	server: { poolSize: 5 },
-	replset: { rs_name: 'realtimevote' },
-	// user: 'myUserName',
-	// pass: 'myPassword'
+      autoIndex: false,
+      db: {
+          native_parser: true,
+      },
+      server: {
+          poolSize: 5
+      }
   }
-
 };
